4cb1153b6019da6e38ddc4adb6681f063e497b4d
Gustavo <gustavoalvim85@gmail.com>
2023-04-05 18:21:07 -0300

    update filter checkbox type jobs

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -5,7 +5,7 @@ const TypeJob = () => {
 
   const [option, setOption] = useState('');
 
-  function handleRadioChange(event) {
+  function handleCheckboxChange(event) {
     setOption(event.target.value)
   }
 
@@ -13,26 +13,26 @@ const TypeJob = () => {
     <div className='typeJob'>
       <h4>Tipo de vaga</h4>
       <div>
-        <label htmlFor='radio-remoto'>
+        <label htmlFor='checkbox-remoto'>
           <input 
             className='checkMark'
-            type='radio'
+            type='checkbox'
             name='type_job'
             checked={option === 'remoto'}
-            onChange={handleRadioChange}
-            id='radio-remoto'
+            onChange={handleCheckboxChange}
+            id='checkbox-remoto'
             value='remoto'
           />
           <span>Remoto</span>
         </label>
-        <label htmlFor='radio-presencial'>
+        <label htmlFor='checkbox-presencial'>
           <input 
             className='checkMark'
-            type='radio'
+            type='checkbox'
             name='type_job'
             checked={option === 'presencial'}
-            onChange={handleRadioChange}
-            id='radio-presencial'
+            onChange={handleCheckboxChange}
+            id='checkbox-presencial'
             value='presencial'
           />
           <span>Presencial</span>
